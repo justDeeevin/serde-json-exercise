7b4639b7fbb63e43dc3644cfe7f75b1b26e7d7c6
Devin Droddy <devin.droddy@gmail.com>
2025-05-29 22:21:19 -0400

    re-export `from_reader` and `from_bytes`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod de;
-pub use de::{Deserializer, from_str};
+pub use de::{Deserializer, from_bytes, from_reader, from_str};
 pub mod ser;
 pub use ser::{Serializer, to_bytes, to_string, to_writer};
 pub mod error;
